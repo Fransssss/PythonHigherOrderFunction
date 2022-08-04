a87ede86b053f89c9d199caf84ecaed98cf47161
Frans <97081479+Fransssss@users.noreply.github.com>
2022-08-04 09:43:12 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+
+# =================
+# practice send function as argument in another function
+# =================
+
+def loud(l_text):
+    return l_text.upper()
+
+
+def quiet(q_text):
+    return q_text.lower()
+
+
+def the_text(a_function):               # after this is called, it is going to ask user for input / text
+    input_text = input("Enter a text: ").capitalize()
+    this_text = a_function(input_text)  # any function that user want the text to be displayed will be called here
+    return this_text
+
+
+print("\n== Say something... ==")
+print("1. LOUDLY")
+print("2. quietly")
+choice = input("choice: ").lower()       # make user input to lower case to make while-loop condition simpler
+
+while choice != 'e':
+    if choice == '1':
+        print("")
+        print(the_text(loud))           # the_text function in this case is in higher order / will be called first
+
+    elif choice == '2':
+        print("")
+        print(the_text(quiet))
+
+    else:
+        print("\n[ Invalid choice ]")
+
+    print("\n== Say something ==")
+    print("1. Loudly")
+    print("2. Quietly")
+    choice = input("choice: ").lower()
+
+print("\n== Exit Program ==")
